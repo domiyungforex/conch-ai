64ec093cc50e77c96752f8fd548c1b25e3da674a
oousug <domiyungforex@gmail.com>
2026-09-02 01:10:54 +0100

    fix: remove remaining the-way files that break the build

--- src/db/schema/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './users';
-export * from './bible';
-export * from './embeddings';
-export * from './knowledge';
-export * from './user-content';
-export * from './settings';
-export * from './challenge';
